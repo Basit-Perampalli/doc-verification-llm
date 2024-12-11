e0db911852faed1db3161c6c0efad8944d641d93
Basit-Perampalli <apeerampalli@gmail.com>
2024-12-12 02:45:59 +0530

    added some urls in backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("authentication.urls")),
     path("verify/", include("verify.urls")),
+    path("applicantform/", include("application.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
